cce68eb3e67bcaa0ce69cfd21c2260cec91cef83
Falk Tandetzky <falk.tandetzky@accenture.com>
2025-09-08 14:49:53 +0200

    some cleanup

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import indexRouter from './routes/index.js';
-import apiRouter from './routes/api.js';
+import apiRouter from './api/index.js';
+import http from 'http';
+
+const port = process.env.PORT || '3000';
 
 const app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 app.use(express.static('public'));
-
-app.use('/', indexRouter);
 app.use('/api', apiRouter);
+app.set('port', port);
 
-export default app;
+const server = http.createServer(app);
+server.listen(port);
--- public/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-const main = async () => {
-    const response = await fetch('/api')
-    const data = await response.json()
-
-    document.getElementById('test-api').innerText = data.message;
-}
-
-
-main();
