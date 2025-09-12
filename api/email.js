0371cdffe69b8c39d9daf64fd54e88faab2502a8
Falk Tandetzky <falk.tandetzky@accenture.com>
2025-09-12 22:21:00 +0200

    email via smtp

--- api/email.js
+++ api/email.js
@@ -1,24 +1,28 @@
 import 'dotenv/config';
+import nodemailer from 'nodemailer';
 
 
 export async function sendMail(to, subject, body) {
-    const response = await fetch('https://send.api.mailtrap.io/api/send', {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${process.env.MAILTRAP_API_TOKEN}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            from: { email: 'hello@demomailtrap.co', name: 'Mailtrap Test' },
-            to: [{ email: to }],
-            subject: subject,
-            text: body,
-        })
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
     });
 
-    if (!response.ok) {
-        const errorText = await response.text();
-        console.error('Error response from email API:', {errorText, status: response.status});
-        throw new Error(`Failed to send email: ${response.status} - ${errorText}`);
+    const mailOptions = {
+        from: process.env.SMTP_FROM,
+        to: to,
+        subject: subject,
+        text: body
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        console.error('Error sending email via SMTP:', error);
+        throw new Error(`Failed to send email via SMTP: ${error.message}`);
     }
 }
