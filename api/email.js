e25ad4481e0de4697c6ac1e5885c39b32742a819
Falk Tandetzky <falk.tandetzky@accenture.com>
2025-09-12 22:50:48 +0200

    smtp secure, update .env.example

--- api/email.js
+++ api/email.js
@@ -6,6 +6,7 @@ export async function sendMail(to, subject, body) {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
+        secure: true,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
