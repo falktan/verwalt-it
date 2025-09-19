dd357d436655a294aeb7e77ea3d9eadc3a931275
Falk Tandetzky <falk.tandetzky@accenture.com>
2025-09-19 23:10:12 +0200

    introduce custom components

---
+++ public/main/verwalt-it-content-wrapper.js
@@ -0,0 +1,40 @@
+(() => {
+    class ContentWrapper extends HTMLElement {
+        constructor() {
+            super();
+        }
+        connectedCallback() {
+            this.attachShadow({mode: 'open'});
+            this.shadowRoot.innerHTML = this.render();
+        }
+
+        render() {
+            return `
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@latest/css/pico.classless.min.css">
+                <link rel="stylesheet" href="/style.css">
+
+                <div class="content-wrapper" id="content-wrapper">
+                    <div class="nav-bar">
+                        <a href="/">
+                            <slot name="logo">
+                                <!-- Default logo if none is provided via slot -->
+                                <img class="logo" src="assets/logo.png" alt="Logo">
+                            </slot>
+                        </a>
+                        <br/> <br/>
+                        <a style="margin: 1em;" href="/">Startseite</a>
+                        <a style="margin: 1em;" href="/impressum.html">Impressum</a>
+                        <a style="margin: 1em;" href="/datenschutzerklaerung.html">Datenschutz</a>
+                    </div>
+                    <div class="main-content" id="main-content">
+                        <slot></slot>
+                    </div>
+                    <div class="footer" id="footer">
+                    </div>
+                </div>
+            `;
+        }
+    }
+
+    customElements.define('verwalt-it-content-wrapper', ContentWrapper);
+})();
