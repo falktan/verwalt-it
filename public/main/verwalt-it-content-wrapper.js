88dcc6c1f6eb5965e1d5d4eee34a4ecd4f17ead8
Falk Tandetzky <falk.tandetzky@accenture.com>
2025-09-22 17:43:00 +0200

    rework styling of form

--- public/main/verwalt-it-content-wrapper.js
+++ public/main/verwalt-it-content-wrapper.js
@@ -10,7 +10,6 @@
 
         render() {
             return `
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@latest/css/pico.classless.min.css">
                 <link rel="stylesheet" href="/style.css">
 
                 <div class="content-wrapper" id="content-wrapper">
@@ -23,16 +22,19 @@
                         </a>
 					<br/>
                     </div>
+
                     <div class="main-content" id="main-content">
                         <slot></slot>
                     </div>
-					<div class="footer" id="footer">
+                    <hr/>
+
+                    <div class="footer" id="footer">
 						<div class="nav-links">
                             <a href="/">Startseite</a>
                             <a href="/hochschule-schmalkalden/fak-elektrotechnik/antrag-ausgabe-batchelorarbeit">FH Schmalkalden</a>
                             <a href="/impressum">Impressum</a>
                             <a href="/datenschutzerklaerung">Datenschutz</a>
-                            <a href="https://github.com/falktan/verwalt-it">Verwalt-It auf GitHub</a>
+                            <a href="https://github.com/falktan/verwalt-it" target="_blank">Verwalt-It auf GitHub</a>
 						</div>
 					</div>
                 </div>
