ac2ca23e021aafe3f3968a823cb0f62fe7ad65f6
Falk Tandetzky <falk.tandetzky@accenture.com>
2025-09-22 16:39:26 +0200

    fix minor styling issue

--- public/main/verwalt-it-content-wrapper.js
+++ public/main/verwalt-it-content-wrapper.js
@@ -30,8 +30,8 @@
 						<div class="nav-links">
                             <a href="/">Startseite</a>
                             <a href="/hochschule-schmalkalden/fak-elektrotechnik/antrag-ausgabe-batchelorarbeit">FH Schmalkalden</a>
-                                <a href="/impressum">Impressum</a>
-                                <a href="/datenschutzerklaerung">Datenschutz</a>
+                            <a href="/impressum">Impressum</a>
+                            <a href="/datenschutzerklaerung">Datenschutz</a>
                             <a href="https://github.com/falktan/verwalt-it">Verwalt-It auf GitHub</a>
 						</div>
 					</div>
