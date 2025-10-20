e0f51f273df2410251d2240c8637f5a3c7758fe9
Falk Tandetzky <falk.tandetzky@accenture.com>
2025-10-20 19:41:38 +0200

    block urls to prevent spam

--- api/utils/validationSchemas.js
+++ api/utils/validationSchemas.js
@@ -1,34 +1,45 @@
 import * as yup from 'yup';
 
+// URL-Validierung für Spam-Vermeidung
+const urlRegex = /(https?:\/\/|www\.|ftp:\/\/)/i;
+const noUrlValidation = yup.string().test(
+  'no-urls',
+  'URLs sind in diesem Feld nicht erlaubt',
+  function(value) {
+    if (!value || value.trim() === '') return true; // Leere Werte sind erlaubt
+    return !urlRegex.test(value);
+  }
+);
+
 // Basis-Schema für gemeinsame Felder
 const baseFormDataSchema = {
-  nachname: yup.string().required('Nachname ist erforderlich'),
-  vorname: yup.string().required('Vorname ist erforderlich'),
-  geburtsdatum: yup.string().required('Geburtsdatum ist erforderlich'),
-  geburtsort: yup.string().required('Geburtsort ist erforderlich'),
-  matrikelnummer: yup.string().required('Matrikelnummer ist erforderlich'),
-  plz_ort: yup.string().required('PLZ/Ort ist erforderlich'),
-  wohnanschrift: yup.string().required('Wohnanschrift ist erforderlich'),
+  nachname: noUrlValidation.required('Nachname ist erforderlich'),
+  vorname: noUrlValidation.required('Vorname ist erforderlich'),
+  geburtsdatum: noUrlValidation.required('Geburtsdatum ist erforderlich'),
+  geburtsort: noUrlValidation.required('Geburtsort ist erforderlich'),
+  matrikelnummer: noUrlValidation.required('Matrikelnummer ist erforderlich'),
+  plz_ort: noUrlValidation.required('PLZ/Ort ist erforderlich'),
+  wohnanschrift: noUrlValidation.required('Wohnanschrift ist erforderlich'),
   email: yup.string().email('Ungültige E-Mail-Adresse').required('E-Mail ist erforderlich'),
-  studiengang: yup.string().required('Studiengang ist erforderlich'),
-  vertiefungsrichtung: yup.string().default(''),
-  thema: yup.string().required('Thema ist erforderlich'),
+  studiengang: noUrlValidation.required('Studiengang ist erforderlich'),
+  vertiefungsrichtung: noUrlValidation.default(''),
+  thema: noUrlValidation.required('Thema ist erforderlich'),
   erste_bachelorarbeit: yup.string().oneOf(['ja', 'nein'], 'Ungültige Auswahl').required('Dieses Feld ist erforderlich'),
   einzelarbeit: yup.string().oneOf(['ja', 'nein'], 'Ungültige Auswahl').required('Dieses Feld ist erforderlich'),
-  weitere_bearbeiter: yup.string().default(''),
-  noch_zu_erbringen: yup.string().default(''),
-  unternehmen_institution: yup.string().required('Unternehmen/Institution ist erforderlich'),
-  arbeitsort: yup.string().required('Arbeitsort ist erforderlich'),
-  betreuer_betrieblich_name: yup.string().required('Name des betrieblichen Betreuers ist erforderlich'),
-  betreuer_betrieblich_tel: yup.string().required('Telefon des betrieblichen Betreuers ist erforderlich'),
+  weitere_bearbeiter: noUrlValidation.default(''),
+  noch_zu_erbringen: noUrlValidation.default(''),
+  unternehmen_institution: noUrlValidation.required('Unternehmen/Institution ist erforderlich'),
+  arbeitsort: noUrlValidation.required('Arbeitsort ist erforderlich'),
+  betreuer_betrieblich_name: noUrlValidation.required('Name des betrieblichen Betreuers ist erforderlich'),
+  betreuer_betrieblich_tel: noUrlValidation.required('Telefon des betrieblichen Betreuers ist erforderlich'),
   betreuer_betrieblich_email: yup.string().email('Ungültige E-Mail-Adresse').required('E-Mail des betrieblichen Betreuers ist erforderlich'),
-  hochschulbetreuer_grad: yup.string(),
-  hochschulbetreuer_name: yup.string().required('Name des Hochschulbetreuers ist erforderlich'),
-  hochschulbetreuer_tel: yup.string().required('Telefon des Hochschulbetreuers ist erforderlich'),
+  hochschulbetreuer_grad: noUrlValidation,
+  hochschulbetreuer_name: noUrlValidation.required('Name des Hochschulbetreuers ist erforderlich'),
+  hochschulbetreuer_tel: noUrlValidation.required('Telefon des Hochschulbetreuers ist erforderlich'),
   hochschulbetreuer_email: yup.string().email('Ungültige E-Mail-Adresse').required('E-Mail des Hochschulbetreuers ist erforderlich'),
-  korreferent_grad: yup.string(),
-  korreferent_name: yup.string().required('Name des Korreferenten ist erforderlich'),
-  korreferent_tel: yup.string().required('Telefon des Korreferenten ist erforderlich'),
+  korreferent_grad: noUrlValidation,
+  korreferent_name: noUrlValidation.required('Name des Korreferenten ist erforderlich'),
+  korreferent_tel: noUrlValidation.required('Telefon des Korreferenten ist erforderlich'),
   korreferent_email: yup.string().email('Ungültige E-Mail-Adresse').required('E-Mail des Korreferenten ist erforderlich'),
   datenschutz_zustimmung: yup.string().oneOf(['on'], 'Datenschutzerklärung muss akzeptiert werden').required('Datenschutzerklärung muss akzeptiert werden')
 };
@@ -36,7 +47,7 @@ const baseFormDataSchema = {
 // Schema für erste Erstellung durch Studenten (Prüfungsamt-Felder optional)
 const formDataSchemaInitial = yup.object({
   ...baseFormDataSchema,
-  pruefungsleistungen_noch_zu_erbringen: yup.string().default(''),
+  pruefungsleistungen_noch_zu_erbringen: noUrlValidation.default(''),
   immatrikulation_laufend: yup.string().oneOf(['ja', 'nein', ''], 'Ungültige Auswahl').default(''),
   zulassung_praxissemester: yup.string().oneOf(['ja', 'nein', ''], 'Ungültige Auswahl').default('')
 }).noUnknown(true, 'Unbekannte Felder sind nicht erlaubt');
@@ -44,7 +55,7 @@ const formDataSchemaInitial = yup.object({
 // Schema für Updates durch Prüfungsamt (Prüfungsamt-Felder Pflicht)
 const formDataSchemaUpdate = yup.object({
   ...baseFormDataSchema,
-  pruefungsleistungen_noch_zu_erbringen: yup.string().default(''),
+  pruefungsleistungen_noch_zu_erbringen: noUrlValidation.default(''),
   immatrikulation_laufend: yup.string().oneOf(['ja', 'nein'], 'Ungültige Auswahl').required('Immatrikulation im laufenden Semester ist erforderlich'),
   zulassung_praxissemester: yup.string().oneOf(['ja', 'nein'], 'Ungültige Auswahl').required('Zulassung zum Praxissemester ist erforderlich')
 }).noUnknown(true, 'Unbekannte Felder sind nicht erlaubt');
